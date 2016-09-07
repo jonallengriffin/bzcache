e613ebb1a4ca7d8a3f269d8154d271d3fb622b0f
Ed Morley <emorley@mozilla.com>
2016-09-07 22:16:18 +0100

    Bug 1286935 - Stop manually loading OF to populate the cache
    Since this should be redundant given `index_bugs_by_keyword()` runs
    prior to it.

--- bzcache/bz_cache_refresh.py
+++ bzcache/bz_cache_refresh.py
@@ -2,10 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import datetime
-import urllib
-import urlparse
-
 from pyes import ES
 from pyes.exceptions import ElasticSearchException
 
@@ -27,24 +23,6 @@ def main(options):
     bzcache = BugzillaCache(es_server=options.es_server)
     bzcache.index_bugs_by_keyword('intermittent-failure')
 
-    # Now, request an API from the WOO server that will cause all the bugs
-    # relevant to WOO to be re-inserted into the cache.
-
-    # calculate the startday and endday parameters
-    today = datetime.date.today()
-    earlier = today - datetime.timedelta(60)
-    of_parsed_url = urlparse.urlparse(options.of_server)
-    of_base_url = '%s://%s%s%s' % (of_parsed_url.scheme or 'http',
-                                   of_parsed_url.netloc,
-                                   of_parsed_url.path,
-                                   '/' if of_parsed_url.path[-1] != '/' else '')
-    of_url = "%sbybug?startday=%s&endday=%s&tree=All" % \
-        (of_base_url, str(earlier), str(today))
-    print datetime.datetime.now(), of_url
-
-    # retrieve the url
-    data = urllib.urlopen(of_url)
-    data.read()
 
 if __name__ == "__main__":
   import optparse
